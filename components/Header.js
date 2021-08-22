25b225fd999aecd5115813af4412376d617130f0
Isreal6ix <isrealsix@gmail.com>
2021-08-23 03:59:18 +0700

    completed the Header component

--- components/Header.js
+++ components/Header.js
@@ -21,9 +21,32 @@ const Header = () => {
 					/>
 				</div>
 
-				<div className="bg-red-700">
-					<SearchIcon className="h-6 text-black" />
-					<input type="text" placeholder="test" className="bg-black" />
+				<div className="hidden  sm:flex items-center bg-yellow-400 h-10 rounded-md flex-grow hover:bg-yellow-500 cursor-pointer">
+					<input
+						className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+						type="text"
+					/>
+					<SearchIcon className="h-12 p-4" />
+				</div>
+				{/* Right */}
+				<div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+					<div className="link">
+						<p>Hello Israel Ojeifo</p>
+						<p className="font-extrabold md:text-sm">Account & lists</p>
+					</div>
+					<div className="link">
+						<p>Returns</p>
+						<p className="font-extrabold md:text-sm">& Orders</p>
+					</div>
+					<div className="relative link flex items-center">
+						<span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+							0
+						</span>
+						<ShoppingCartIcon className="h-10" />
+						<p className="hidden md:inline font-extrabold md:text-sm mt-2">
+							Basket
+						</p>
+					</div>
 				</div>
 			</div>
 
