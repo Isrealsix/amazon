15f3f433e4e833235d5b1b6833eb1878d29d456c
Isreal6ix <isrealsix@gmail.com>
2021-08-23 02:53:41 +0700

    vagitus

---
+++ components/Header.js
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import {
+	MenuIcon,
+	SearchIcon,
+	ShoppingCartIcon,
+} from '@heroicons/react/outline';
+
+const Header = () => {
+	return (
+		<header>
+			{/* Top */}
+			<div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+				<div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+					<Image
+						src="https://links.papareact.com/f90"
+						width={150}
+						height={40}
+						alt="logo"
+						objectFit="contain"
+						className="cursor-pointer"
+					/>
+				</div>
+
+				<div>
+					<input type="text" />
+				</div>
+			</div>
+
+			{/* Bottom */}
+			<div></div>
+		</header>
+	);
+};
+
+export default Header;
