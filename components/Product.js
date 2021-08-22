b38bfd8fa8927bad2434b75f66929b17293c12ea
Isreal6ix <isrealsix@gmail.com>
2021-08-23 06:13:59 +0700

    completed the products and productsFeed

---
+++ components/Product.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import Image from 'next/dist/client/image';
+import { StarIcon } from '@heroicons/react/solid';
+import Currency from 'react-currency-formatter';
+
+const MAX_N = 5;
+const MIN_N = 1;
+const Product = ({ title, id, price, description, category, image }) => {
+	const [rating] = useState(
+		Math.floor(Math.random() * (MAX_N - MIN_N + 1)) + MIN_N
+	);
+	const [isPrime] = useState(Math.random() < 0.5);
+	return (
+		<div className="relative flex flex-col m-5 bg-white z-30 p-10">
+			<p className="absolute top-2 right-2 text-xs italic text-gray-400">
+				{category}
+			</p>
+			<Image src={image} height={200} width={200} objectFit="contain" />
+			<h4 className="my-3">{title}</h4>
+			<div className="flex">
+				{Array(rating)
+					.fill()
+					.map((_, idx) => (
+						<StarIcon key={idx} className="h-5 text-yellow-500" />
+					))}
+			</div>
+			<p className="text-xs my-2 line-clamp-2">{description}</p>
+
+			<div className="mb-5">
+				<Currency quantity={price} currency="RUB" />
+			</div>
+
+			{isPrime && (
+				<div className="flex items-center space-x-2 -mt-5">
+					<img
+						className="w-12"
+						src="https://links.papareact.com/fdw"
+						alt="primed"
+					/>
+					<p className="text-xs text-gray-500">FRREE NEXT-day Delivery</p>
+				</div>
+			)}
+			<button className="mt-auto button">Add to Basket</button>
+		</div>
+	);
+};
+
+export default Product;
---
+++ components/ProductFeed.js
@@ -0,0 +1,57 @@
+import Product from './Product';
+const ProductFeed = ({ products }) => {
+	return (
+		<div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+			{products
+				.slice(0, 4)
+				.map(({ title, id, price, description, category, image }) => (
+					<Product
+						id={id}
+						key={id}
+						title={title}
+						price={price}
+						description={description}
+						image={image}
+						category={category}
+					/>
+				))}
+
+			<img
+				className="md:col-span-full"
+				src="https://links.papareact.com/dyz"
+				alt=""
+			/>
+
+			<div className="md:col-span-2">
+				{products
+					.slice(4, 5)
+					.map(({ title, id, price, description, category, image }) => (
+						<Product
+							id={id}
+							key={id}
+							title={title}
+							price={price}
+							description={description}
+							image={image}
+							category={category}
+						/>
+					))}
+			</div>
+
+			{products
+				.slice(5)
+				.map(({ title, id, price, description, category, image }) => (
+					<Product
+						key={id}
+						title={title}
+						price={price}
+						description={description}
+						image={image}
+						category={category}
+					/>
+				))}
+		</div>
+	);
+};
+
+export default ProductFeed;
