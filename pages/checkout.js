0b37591a15ebfcec706db8a39327bf1c562e1f52
Isreal6ix <isrealsix@gmail.com>
2021-08-23 20:45:32 +0700

    styled the right part

--- pages/checkout.js
+++ pages/checkout.js
@@ -3,8 +3,11 @@ import { useSelector } from 'react-redux';
 import Header from '../components/Header';
 import { selectItems } from '../slices/basketSlice';
 import CheckoutProduct from '../components/CheckoutProduct';
+import { Currency } from 'react-currency-formatter';
+import { useSession } from 'next-auth/client';
 const Checkout = () => {
 	const items = useSelector(selectItems);
+	const session = useSession();
 	return (
 		<div>
 			<Header />
@@ -39,7 +42,28 @@ const Checkout = () => {
 				</div>
 
 				{/* Right */}
-				<div></div>
+				<div className="flex flex-col bg-white p-10 shadow-md">
+					{items.length > 0 && (
+						<>
+							<h2 className="whitespace-nowrap">
+								Subtotal ({items.length} items):{' '}
+								<span className="font-bold">
+									<Currency quantity={total} currency="RUB" />
+								</span>
+							</h2>
+
+							<button
+								disabled={!session}
+								className={`button mt-2 ${
+									!session &&
+									'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'
+								}`}
+							>
+								{!session ? 'Sign in to checkout' : 'Proceed to checkout'}
+							</button>
+						</>
+					)}
+				</div>
 			</main>
 		</div>
 	);
